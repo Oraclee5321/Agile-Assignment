c6719962b0bf46666964ee29b13741f3352f3aa7
Alex Cooper <98158236+Oraclee5321@users.noreply.github.com>
2025-03-28 12:09:47 +0000

    Draggable widgets

--- Kivy-Dev/Drag-n-Drop/drag.py
+++ Kivy-Dev/Drag-n-Drop/drag.py
@@ -10,6 +10,8 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import StringProperty, BooleanProperty, NumericProperty
 from kivy.modules import inspector
 from kivy.utils import platform
+from kivy.uix.behaviors import DragBehavior
+from kivy.uix.label import Label
 
 from kivymd.app import MDApp
 from kivymd.uix.card import MDCard
@@ -18,15 +20,42 @@ from kivymd.uix.screenmanager import MDScreenManager
 import random
 import os
 
-class DragGameScreen(MDScreen):
-     score = NumericProperty(0)
-     def
-    pass
+class DragLabel(DragBehavior, Label):
+    text = StringProperty()
+    id = StringProperty()
+
+
+
+class DragScreen(MDScreen):
+    score = NumericProperty(0)
+
+    def generate_drag_boxes(self):
+        for x in range(5):
+            new_label = DragLabel()
+            new_label.text = str(x)
+            new_label.size_hint = (0.25, 0.2)
+            self.ids.grid.add_widget(new_label)
+        for item in self.ids.grid.children:
+            item.pos = random.randint(0, 500), random.randint(0, 500)
+
+
+
+
+
+
+    def generate_answer_boxes(self):
+        pass
+
+    def on_enter(self, *args):
+        self.ids.grid.clear_widgets()
+        self.generate_drag_boxes()
+        self.generate_answer_boxes()
+
 
 class DragGameApp(MDApp):
     def build(self):
         self.screens = MDScreenManager()
-        self.screens.add_widget(DragGameScreen(name='home'))
+        self.screens.add_widget(DragScreen(name='home'))
         # size of home
         Window.size = (500, 500)
         inspector.create_inspector(Window, self.screens)
