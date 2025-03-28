283a5bbf8d7c0226b37484e85b134a07469735a6
SirBuddyNoah <151926735+SirBuddyNoah@users.noreply.github.com>
2025-03-28 10:13:19 +0000

    Create Finger-Name.py
    creating a a kivy that takes text input,

---
+++ Kivy-Dev/Finger-Name.py
@@ -0,0 +1,27 @@
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.textinput import Textinput 
+from kivy.uix.relativelayout import RelativeLayout
+
+# Create the widget
+class text(Widget): 
+    pass
+  
+# Create the app
+class MainApp(App): 
+  
+    # Building text input 
+    def build(self): 
+        return text() 
+  
+    # Arranging that what you write will be shown to you 
+    # in IDLE 
+    def process(self): 
+        text = self.root.ids.input.text 
+        print(text) 
+  
+# Run the App 
+if __name__ == "__main__": 
+    MainApp().run() 
+
